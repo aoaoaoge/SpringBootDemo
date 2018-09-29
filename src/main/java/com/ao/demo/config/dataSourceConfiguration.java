9d4229ff9f4134889a88a9c1208ab93e39efdac0
敖裕欢 <1071934670@qq.com>
2018-09-29 10:49:49 +0800

    数据库连接配置

--- src/main/java/com/ao/demo/config/dataSourceConfiguration.java
+++ src/main/java/com/ao/demo/config/dataSourceConfiguration.java
@@ -1,4 +1,16 @@
 package com.ao.demo.config;
 
+import com.alibaba.druid.pool.DruidDataSource;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
 public class dataSourceConfiguration {
+
+    @ConfigurationProperties(prefix = "spring.datasource")
+    @Bean(name = "dataSource")
+    public DruidDataSource createDataSource(){
+        DruidDataSource dataSource = new DruidDataSource();
+        return dataSource;
+    }
+
 }
